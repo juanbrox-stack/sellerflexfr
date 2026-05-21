0dce66ef558ecda95fbc4c1101f9626254fe6125
juanbrox-stack <juanbrox@cecotec.es>
2026-05-21 10:00:59 +0200

    Change recipient email for order cancellation

--- sellerflexfr.py
+++ sellerflexfr.py
@@ -109,7 +109,7 @@ def enviar_correo_background(excel_data, filename):
         sender_email = st.secrets["email"]["sender_email"]
         sender_password = st.secrets["email"]["sender_password"]
         
-        destinatario = "juanbrox@cecotec.es"
+        destinatario = "fr-sellerflex-support@amazon.com"
         fecha_hoy = datetime.now().strftime("%d/%m/%Y")
         asunto = f"Cancel orders {fecha_hoy}"
         cuerpo_mensaje = "Good morning,\n\nI attach one order to cancel.\n\nBest regards."
